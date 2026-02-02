83c834bc111e6ecde2e666713a8b6a17cf82a614
JosiasHA <clubjosias2@gmail.com>
2026-02-02 17:01:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 meme_dict = {
             "CRINGE": "Algo excepcionalmente raro o embarazoso",
             "LOL": "Una respuesta común a algo gracioso",
+            "BRUH": "sorpresa desaprovacion decepcion es como decir enserio? eso es",
+            "XD": "que da risa o es gracioso",
+            "NAH": "desacuerdo",
+            "BRO": "amigo, hermano, persona de confiansa amistosa",
+            "STALKER": "acosador, o persona que sigue a otra de forma denunciable",
+            "MOOD": "la emocion del momento",
+            "GOAT": "que es el mejor en algo especifico",
+            "BASADO": "persona que dice lo que cree sin miedo a que lo juzgen",
             }
 
-word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+word = input("Escribe una palabra que no entiendas entre estas palabras (¡con mayúsculas!): CRINGE, LOL, XD, BRUH, NAH, BRO, STALKER, MOOD, GOAT, BASADO")
 
 if word in meme_dict.keys():
     # ¿Qué debemos hacer si se encuentra la palabra?
